d0b1f9d2cb2c3e8db896fd537357082b66974239
Shivam Aggarwal <shivam.aggarwal1597@gmail.com>
2018-02-02 23:38:12 +0530

    Second

--- app/src/main/java/com/example/shivam/dellproject/BarcodeActivity.java
+++ app/src/main/java/com/example/shivam/dellproject/BarcodeActivity.java
@@ -1,6 +1,7 @@
 package com.example.shivam.dellproject;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -17,6 +18,7 @@ public class BarcodeActivity extends AppCompatActivity {
     EditText editText;
     Button submit_button,scan_barcode;
     private ZXingScannerView zXingScannerView;
+    private static final String CODE = "error";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +40,17 @@ public class BarcodeActivity extends AppCompatActivity {
 
             }
         });
+        submit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String check_text = editText.getText().toString();
+                check_text = perform_validation(check_text);
+                if (!check_text.equals(CODE)){
+                    Intent intent = new Intent(BarcodeActivity.this,NavigatorActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
 
 
     }
@@ -54,7 +67,24 @@ public class BarcodeActivity extends AppCompatActivity {
         public void handleResult(com.google.zxing.Result result) {
             String result_code = result.getText();
             Toast.makeText(BarcodeActivity.this,result_code,Toast.LENGTH_LONG).show();
-            setContentView(R.layout.activity_main);
+            setContentView(R.layout.activity_barcode);
+            editText.setText(result_code);
         }
+    }
+    public String perform_validation(String text){
+        //TODO : Perform Validations Accordingly
+        if (text.trim().length()<4){
+            return CODE;
+        }
+        else {
+            return text;
+        }
+
+    }
+    @Override
+    public void  onPause(){
+
+        super.onPause();
+        //zXingScannerView.stopCamera();
     }
 }
